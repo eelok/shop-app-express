3afaecb414e114a976526fe4c0e36d61fd72ca84
eelok <maria@resourceful-humans.de>
2022-08-20 10:39:56 +0200

    add post request

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/add-product", (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
     console.log(req.body);
-    res.redirect("/xyi");
+    res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
